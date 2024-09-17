e63d3bfd43a22c8d7f40038e50ef87c5984ca004
YuraSich <metelkiny98@gmail.com>
2024-09-17 17:56:36 +0300

    добавил шифровку дешифровку

--- TestSign_2/CryptoProService.cs
+++ TestSign_2/CryptoProService.cs
@@ -1,10 +1,18 @@
-﻿using System.Runtime.InteropServices;
+﻿using CryptoPro.Security.Cryptography;
+using CryptoPro.Security.Cryptography.Pkcs;
 using CryptoPro.Security.Cryptography.X509Certificates;
+using CryptoPro.Security.Cryptography.Xml;
+using SignTestApp;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Security.Cryptography.Pkcs;
 using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Xml;
 
-namespace SignTestApp;
+namespace CryptoModuleTest;
 
-internal class CryptoProService : ICryptoService
+internal class CryptoProService
 {
     private const int CRYPT_VERIFYCONTEXT = -268435456; //No private key access required
 
@@ -12,7 +20,7 @@ internal class CryptoProService : ICryptoService
 
     private static readonly CryptoProVersion MinimalSupportedVersion = new(5, 0);
 
-    public CpX509Certificate2? GetCert(string? email)
+    public CpX509Certificate2? GetCert(string email)
     {
         var cryptoProVersion = GetCryptoProInstalledVersion();
         if (cryptoProVersion == null)
@@ -39,15 +47,193 @@ internal class CryptoProService : ICryptoService
         return irmCertificates.MaxBy(x => x.NotAfter);
     }
 
-    public CpX509Certificate2? CreateCert(string cn, string? email)
+    /// <summary>
+    /// Формирование ключа шифрования и подписи.
+    /// ??? Функция CryptGenKey генерирует случайные криптографические ключи или ключевую пару(закрытый/открытый ключи). 
+    /// Полученный дескриптор ключа должен в обязательном порядке быть удалён
+    /// с помощью вызова функции CryptDestroyKey до вызова функции CryptReleaseContext для рабочего дескриптора криптопровайдера.
+    /// 
+    /// + Криптопровайдер - Crypto-Pro GOST R 34.10-2012 Cryptographic Service Provider;
+    /// + Алгоритм генерации ключа - ГОСТ 34.10-2012 256 бит;
+    /// Назначение ключа - Подпись и шифрование;
+    /// Параметры экспорта - Не экспортируемые;
+    /// + Способы использования ключа - Подпись(digitalSignature) Шифрование(dataEncryption), Согласование(keyAgreement);
+    /// + Назначение сертификата - Проверка подлинности клиента.
+    /// ? Срок действия открытого ключа - 1 год.
+    /// ? Срок действия закрытого ключа - 4 года.
+    /// </summary>
+    public CpX509Certificate2 GenerateCertificate(string cn, string email)
     {
-        throw new NotImplementedException();
+        var gost3410 = Gost3410_2012_256.Create();
+
+        var builder = new X500DistinguishedNameBuilder();
+        builder.AddCommonName(cn + Postfix);
+        builder.AddEmailAddress(email);
+        var distinguishedName = builder.Build();
+
+        var certificateRequest = new CpCertificateRequest(distinguishedName.Name, gost3410);
+
+        var usageFlags = X509KeyUsageFlags.DigitalSignature |
+            X509KeyUsageFlags.DataEncipherment |
+            X509KeyUsageFlags.KeyAgreement;
+
+        certificateRequest.CertificateExtensions.Add(new CpX509KeyUsageExtension(usageFlags, false));
+
+        certificateRequest.CertificateExtensions.Add(new CpX509BasicConstraintsExtension());
+
+        var oidCollection = new OidCollection {
+            new("1.3.6.1.5.5.7.3.2") //Проверка подлинности клиента (1.3.6.1.5.5.7.3.2)
+        };
+
+        certificateRequest.CertificateExtensions.Add(new CpX509EnhancedKeyUsageExtension(oidCollection, true));
+
+        var cert = certificateRequest.CreateSelfSigned(DateTimeOffset.Now, DateTimeOffset.Now.AddYears(1));
+        //using var readStore = new CpX509Store(StoreLocation.CurrentUser);
+        //readStore.Open(OpenFlags.ReadOnly);
+        //var certCollection = readStore.Certificates;
+        //var issuer = certCollection.Find(X509FindType.FindByThumbprint, "39b2693bedf613f0bbcd5cf8054074791eaf78e3", false)[0];
+
+        //var cert2 = certificateRequest.Create(issuer,DateTimeOffset.Now, DateTimeOffset.Now.AddYears(1), );
+
+        cert = new CpX509Certificate2(cert.Export(X509ContentType.Pfx), "", X509KeyStorageFlags.Exportable);
+
+        using (var store = new CpX509Store(StoreName.My, StoreLocation.CurrentUser))
+        {
+            store.Open(OpenFlags.ReadWrite);
+            store.Add(cert);
+        }
+
+        return cert;
     }
 
-
-    public CpX509Certificate2? GenerateCertificate()
+    public void EnvelopedCmsGost2012_256(CpX509Certificate2 cert)
     {
-        throw new NotImplementedException();
+        // Исходное сообщение.
+        const string msg = "Это сообщение, которое будет зашифровано.";
+
+        Console.WriteLine("{0}Исходное сообщение (длина {1}): {2}  ", Environment.NewLine, msg.Length, msg);
+
+        // Переводим исходное сообщение в массив байтов.
+        var encoding = new UTF8Encoding();
+        var msgBytes = encoding.GetBytes(msg);
+
+        Console.WriteLine("{0}{0}------------------------------", Environment.NewLine);
+        Console.WriteLine(" На стороне отправителя");
+        Console.WriteLine("------------------------------{0}", Environment.NewLine);
+
+        var encodedEnvelopedCms = EncryptMsg(msgBytes, cert, true);
+
+        Console.WriteLine("{0}Сообщение после зашифрования (длина {1}):  ", Environment.NewLine, encodedEnvelopedCms.Length);
+        foreach (var b in encodedEnvelopedCms)
+        {
+            Console.Write("{0:x}", b);
+        }
+
+        File.WriteAllBytes("encrypted.txt.p7e", encodedEnvelopedCms);
+        Console.WriteLine();
+
+        Console.WriteLine("{0}{0}------------------------------", Environment.NewLine);
+        Console.WriteLine(" На стороне получателя  ");
+        Console.WriteLine("------------------------------{0}", Environment.NewLine);
+
+        // Расшифровываем сообщение для одного из получателей
+        // и возвращаем сообщение для отображения.
+        var decryptedMsg = DecryptMsg(encodedEnvelopedCms, cert);
+
+        File.WriteAllText("decrypted.txt", encoding.GetString(decryptedMsg));
+        // Преобразуем расшифрованные байты в сообщение
+        Console.WriteLine("{0}Расшифрованное сообщение: {1}", Environment.NewLine, encoding.GetString(decryptedMsg));
+
+
+        // Зашифровываем сообщение, используя открытый ключ 
+        // получателя, при помощи класса EnvelopedCms.
+        static byte[] EncryptMsg(byte[] msg, CpX509Certificate2 recipientCert, bool useDataContextType)
+        {
+            // Помещаем сообщение в объект ContentInfo 
+            // Это требуется для создания объекта EnvelopedCms.
+            var contentInfo = useDataContextType ?
+                new ContentInfo(new Oid("1.2.840.113549.1.7.1"), msg) :
+                new ContentInfo(ContentInfo.GetContentType(msg), msg);
+
+            // Создаем объект EnvelopedCms, передавая ему
+            // только что созданный объект ContentInfo.
+            // Используем идентификацию получателя (SubjectIdentifierType)
+            // по умолчанию (IssuerAndSerialNumber).
+            // При необходимости использовать другой алгоритм шифрования данных
+            // (отличный от 28147-89 по умолчанию), его AlgorithmIdentifier можно передать вторым параметром.
+            // Например для id-tc26-cipher-gostr3412-2015-magma-ctracpkm (oid: 1.2.643.7.1.1.5.1.1):
+            // CpEnvelopedCms envelopedCms = new CpEnvelopedCms(
+            //    contentInfo, 
+            //    new AlgorithmIdentifier(new Oid("1.2.643.7.1.1.5.1.1")));
+            var envelopedCms = new CpEnvelopedCms(contentInfo);
+
+            // Создаем объект CmsRecipient, который 
+            // идентифицирует получателя зашифрованного сообщения.
+            var recip1 = new CpCmsRecipient(SubjectIdentifierType.IssuerAndSerialNumber, recipientCert);
+
+            Console.Write("Зашифровываем данные для одного получателя с именем {0} ...", recip1.Certificate.SubjectName.Name);
+            // Зашифровываем сообщение.
+            envelopedCms.Encrypt(recip1);
+            Console.WriteLine("Выполнено.");
+
+            // Закодированное EnvelopedCms сообщение содержит
+            // зашифрованный текст сообщения и информацию
+            // о каждом получателе данного сообщения.
+            return envelopedCms.Encode();
+        }
+
+        // Расшифрование закодированного EnvelopedCms сообщения.
+        static byte[] DecryptMsg(byte[] encodedEnvelopedCms, CpX509Certificate2 cert)
+        {
+            // Создаем объект для декодирования и расшифрования.
+            var envelopedCms = new CpEnvelopedCms();
+
+            // Декодируем сообщение.
+            envelopedCms.Decode(encodedEnvelopedCms);
+
+            // Выводим количество получателей сообщения и алгоритм зашифрования.
+            DisplayEnvelopedCms(envelopedCms, false);
+
+            // Расшифровываем сообщение для получателя.
+            Console.Write("Расшифрование ... ");
+            envelopedCms.Decrypt(new CpX509Certificate2Collection(cert));
+            Console.WriteLine("Выполнено.");
+
+            // После вызова метода Decrypt в свойстве ContentInfo 
+            // содержится расшифрованное сообщение.
+            return envelopedCms.ContentInfo.Content;
+        }
+
+        // Отображаем свойство ContentInfo объекта EnvelopedCms 
+        static void DisplayEnvelopedCmsContent(string desc, CpEnvelopedCms envelopedCms)
+        {
+            Console.WriteLine(desc + " (длина {0}):  ", envelopedCms.ContentInfo.Content.Length);
+            foreach (var b in envelopedCms.ContentInfo.Content)
+            {
+                Console.Write(b + " ");
+            }
+            Console.WriteLine();
+        }
+
+        // Отображаем некоторые свойства объекта EnvelopedCms.
+        static void DisplayEnvelopedCms(CpEnvelopedCms e, bool displayContent)
+        {
+            Console.WriteLine("{0}Закодированное CMS/PKCS #7 Сообщение.{0}" + "Информация:", Environment.NewLine);
+            Console.WriteLine("\tАлгоритм шифрования сообщения:{0}", e.ContentEncryptionAlgorithm.Oid.FriendlyName);
+            Console.WriteLine("\tКоличество получателей закодированного CMS/PKCS #7 сообщения:{0}", e.RecipientInfos.Count);
+            for (var i = 0; i < e.RecipientInfos.Count; i++)
+            {
+                Console.WriteLine(
+                    "\tПолучатель #{0} тип {1}.",
+                    i + 1,
+                    e.RecipientInfos[i].RecipientIdentifier.Type);
+            }
+            if (displayContent)
+            {
+                DisplayEnvelopedCmsContent("Закодированное CMS/PKCS #7 содержимое", e);
+            }
+            Console.WriteLine();
+        }
     }
 
     #region private methods
@@ -62,7 +248,14 @@ internal class CryptoProService : ICryptoService
     {
         var dProvider = new nint();
         var version = new byte[4];
-        if (CryptAcquireContext(ref dProvider, null, null, 75, CRYPT_VERIFYCONTEXT))
+        var contextAcquiredSuccessfully = CryptAcquireContext(
+            ref dProvider, // дескриптор CSP
+            null, // имя контейнера
+            null, // использовать поставщика по умолчанию
+            75, // тип поставщика
+            CRYPT_VERIFYCONTEXT); // значения флагов
+
+        if (contextAcquiredSuccessfully)
         {
             uint ll = 4;
             if (CryptGetProvParam(dProvider, 5, version, ref ll, 0))
@@ -72,6 +265,52 @@ internal class CryptoProService : ICryptoService
         }
 
         return null;
+    }
+
+
+    /// <summary>
+    /// Расшифрование на сертификате
+    /// </summary>
+    /// <param name="gostKey"></param>
+    static void Decrypt(string srcName, string destName, CpX509Certificate2 cert)
+    {
+        // Создаем новый объект xml документа.
+        var xmlDoc = new XmlDocument();
+
+        // Пробельные символы участвуют в вычислении подписи и должны быть сохранены для совместимости с другими реализациями
+        xmlDoc.PreserveWhitespace = true;
+
+        // Загружаем в объект созданный XML документ.
+        xmlDoc.Load(srcName);
+
+        // Создаем новый объект CpEncryptedXml по XML документу.
+        var exml = new CpEncryptedXml(xmlDoc);
+
+        // Небольшие хаки, чтобы не устанавливать серт в хранилище
+        {
+            var ns = new XmlNamespaceManager(xmlDoc.NameTable);
+            ns.AddNamespace("ki", "http://www.w3.org/2000/09/xmldsig#");
+            ns.AddNamespace("ek", "http://www.w3.org/2001/04/xmlenc#");
+            var keyName = Convert.ToBase64String(cert.Export(X509ContentType.Cert));
+            var keyInfoNode = xmlDoc.SelectSingleNode("//ki:KeyInfo/ek:EncryptedKey/ki:KeyInfo", ns);
+            if (keyInfoNode == null)
+            {
+                throw new InvalidOperationException("Неверный формат зашифрованного XML-документа.");
+            }
+
+            if (keyInfoNode.InnerText.Equals(keyName, StringComparison.InvariantCultureIgnoreCase))
+            {
+                keyInfoNode.InnerXml = $"<KeyName>{keyName}</KeyName>";
+            }
+            exml.AddKeyNameMapping(keyName, cert.PrivateKey);
+            exml.Recipient = keyName;
+        }
+
+        // Расшифровываем зашифрованные узлы XML документа.
+        exml.DecryptDocument();
+
+        // Сохраняем расшифрованный документ.
+        xmlDoc.Save(destName);
     }
 
     private bool CheckEmail(CpX509Certificate2 cert, string? email) => cert.ExtractEmail()?.Equals(email, StringComparison.OrdinalIgnoreCase) ?? false;
@@ -87,7 +326,19 @@ internal class CryptoProService : ICryptoService
         ref uint dwDataLen,
         uint dwFlags);
 
-    [DllImport("advapi32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+
+    /// <summary>
+    /// Попытаться получить контекст и контейнер ключей.
+    /// Контекст будет использовать CSP по умолчанию для типа поставщика RSA_FULL.
+    /// DwFlags устанавливается в ноль для попытки открыть существующий контейнер ключей.
+    /// </summary>
+    /// <param name="hProv"></param>
+    /// <param name="pszContainer"></param>
+    /// <param name="pszProvider"></param>
+    /// <param name="dwProvType"></param>
+    /// <param name="dwFlags"></param>
+    /// <returns></returns>
+    [DllImport("advapi32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
     private static extern bool CryptAcquireContext(
         ref nint hProv,
         string pszContainer,
--- TestSign_2/Program.cs
+++ TestSign_2/Program.cs
@@ -1,38 +1,25 @@
-﻿namespace SignTestApp;
+﻿using CryptoModuleTest;
+
+namespace SignTestApp;
 
 internal static class Program
 {
     private static void Main()
     {
-        const string certificateCn = "Test Certificate 2";
+        const string certificateCn = "Test Certificate";
         const string email = "test@email.ru";
 
-        var provider = new CryptoProProvider();
+        var provider = new CryptoProService();
 
-        ICryptoService cryptoService = new CryptoProService();
-
-        var inputFilePath = Path.Combine(Environment.CurrentDirectory, "input.txt");
-        var signedFilePathAttached = Path.Combine(Environment.CurrentDirectory, "input.sig");
         try
         {
-            var cert = cryptoService.GetCert(email);
-
-            //CreateFile(inputFilePath);
-            //provider.Sign(inputFilePath, signedFilePathAttached, certificateCn, true);
-            //Console.WriteLine("Документ успешно подписан");
+            var cert = provider.GetCert(email) ?? provider.GenerateCertificate(certificateCn, email);
+            //var cert = provider.GetDefaultCert();
+            provider.EnvelopedCmsGost2012_256(cert);
         }
         catch (Exception ex)
         {
             Console.WriteLine(ex);
-        }
-    }
-
-    private static void CreateFile(string inputFilePath)
-    {
-        if (!File.Exists(inputFilePath))
-        {
-            using var sw = new StreamWriter(inputFilePath);
-            sw.Write("TEST");
         }
     }
 }
